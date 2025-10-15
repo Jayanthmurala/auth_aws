15fd8dc6c219f3a95840210aff3d6924ea72086f
Jayanthmurala <Jayanthmurala@gmail.com>
2025-10-15 15:31:47 +0530

    v.02.3

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # Stop and remove the existing container defined in the docker-compose.yml
+set -e
+
+echo
+
 cd /home/ec2-user/app
-docker compose down || true
+docker compose down || true
+
+echo
+exit 0 
